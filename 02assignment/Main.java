f22d2e9f7b64c77a9aa494a98e29292c90b61846
OpiumSmoke <github@uncloud.lv>
2014-04-24 15:25:57 +0200

    Queens now has a checkPosition method, which determines how many queens threaten the given one

--- 02assignment/Main.java
+++ 02assignment/Main.java
@@ -1,3 +1,4 @@
+import core.*;
 import solvers.*;
 
 public class Main{
@@ -7,9 +8,28 @@ public static void main(String[] args){
 //	BruteForce force = new BruteForce();
 //	force.solve();
 	
-	Backtracking bt = new Backtracking();
+//	Backtracking bt = new Backtracking();
+//	bt.solve();
+	int dim = 8;
 	
-	bt.solve();
+	Heuristic heuristic = new Heuristic(dim);
+	heuristic.solve();
+	
+	Chessboard board = new Chessboard(8);
+//	board.set(true, 3, 0);
+//	board.set(true, 3, 7);
+	board.set(true, 3, 3);
+//	board.set(true, 0, 3);
+//	board.set(true, 7, 3);
+	board.set(true, 1, 1);
+	board.set(true, 5, 5);
+	board.set(true, 5, 1);
+	board.set(true, 0, 6);
+	board.set(true, 0, 0);
+	board.printBoard();
+	
+	Queen queen = new Queen(3, 3);
+	queen.checkPosition(board);
 
 
 }
--- 02assignment/core/Queen.java
+++ 02assignment/core/Queen.java
@@ -1,6 +1,6 @@
 package core;
 
-public class Queen implements Comparable<Object>{
+public class Queen implements Comparable<Object> {
 	int x; // the x coordinate
 	int y; // the y coordinate
 	int threats; // number of queens threatening the current one
@@ -8,30 +8,108 @@ public class Queen implements Comparable<Object>{
 	public Queen(int x, int y) {
 		this.x = x;
 		this.y = y;
+		this.threats = 0;
 	}
-	
+
 	public int getTreats() {
 		return this.threats;
 	}
-/*
- * compares two queens on the basis of the number of threats
- */
+
+	public void resetThreats() {
+		this.threats = 0;
+	}
+
+	/*
+	 * compares two queens on the basis of the number of threats
+	 */
 	@Override
 	public int compareTo(Object queen) {
-		
-		if(!(queen instanceof Queen)){
-            throw new ClassCastException("Invalid object");
-        }
-		
+
+		if (!(queen instanceof Queen)) {
+			throw new ClassCastException("Invalid object");
+		}
+
 		int n = ((Queen) queen).getTreats();
-		
+
 		if (this.getTreats() > n) {
 			return 1;
-		} else if ( this.getTreats() < n) {
+		} else if (this.getTreats() < n) {
 			return -1;
 		} else {
 			return 0;
 		}
 	}
 
+	public int checkPosition(Chessboard board) {
+		int x = this.x;
+		int y = this.y;
+
+		int dim = board.dim;
+
+		// check the column
+		for (int i = 0; i < dim; i++) {
+			if (x != i) {
+				if (board.get(x, i)) {
+					System.out.println("Threat from " + x + " , " + i + " !");
+					this.threats++;
+				}
+			}
+		}
+		// check the row
+		for (int i = 0; i < dim; i++) {
+			if (y != i) {
+				if (board.get(i, y)) {
+					System.out.println("Threat from " + i + " , " + y + " !");
+					this.threats++;
+				}
+			}
+		}
+
+		// check diagonals
+		// to lower right
+		for (int i = x; i < dim; i++) {
+			for (int j = y; j < dim; j++) {
+				if (x != i && y != j) {
+					if (board.get(i, j)) {
+						System.out.println("Threat from " + i + " , " + j
+								+ " !");
+						this.threats++;
+					}
+				}
+			}
+		}
+		// to lower left
+		for (int i = x; i < dim; i++) {
+			for (int j = y; j > 0; j--) {
+				if (x != i && y != j) {
+					if (board.get(i, j)) {
+						System.out.println("Threat from " + i + " , " + j
+								+ " !");
+						this.threats++;
+					}
+				}
+			}
+		}
+		// to upper right
+		for (int i = x, j = y; i >= 0 && j < dim; i--, j++) {
+			 if (i != x && j != y) {
+			if (board.get(i, j)) {
+				System.out.println("Threat from " + i + " , " + j + " !");
+				this.threats++;
+			}
+			 }
+		}
+		// to upper left
+		for (int i = x, j = y; i >= 0 && j >=0; i--, j--) {
+			 if (i != x && j != y) {
+			if (board.get(i, j)) {
+				System.out.println("Threat from " + i + " , " + j + " !");
+				this.threats++;
+			}
+			 }
+		}
+		System.out.println("Queen has " + this.threats + " threats!");
+
+		return this.threats;
+	}
 }
